9d2d54a38be331c85d9dca2af8d5fb0e593b0db6
unknown <a01038517287@gmail.com>
2023-07-17 10:44:31 +0900

    Build topside design, linking

--- FE/haiyeo/src/pages/Home.jsx
+++ FE/haiyeo/src/pages/Home.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
-    <div className="Home">
+    <div className="w-[512px]">
+      <div className="w-[512px] bg-[#F3FFC5] h-[170px]">
+        <div>
+          <img
+            className="w-[200px] absolute mt-[5px] ml-[156px]"
+            alt="hanaEarth"
+            src="/img/hana_earth.png"
+          />
+        </div>
+        <NavLink to="/search">
+          <div className="w-[400px] bg-[#FFFFFF] h-[50px] ml-[56px] absolute mt-[100px] rounded-lg grid content-center">
+            <div className="mx-auto">
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                color="#ababab"
+                className="mr-[15px]"
+              />
+              <span className="text-[#3EC6C9]">하나</span>
+              <span>뿐인 지구를 위한 </span>
+              <span className="text-[#3EC6C9]">E</span>
+              <span>CO </span>
+              <span className="text-[#3EC6C9]">여</span>
+              <span>행</span>
+            </div>
+          </div>
+        </NavLink>
+      </div>
       <p>홈</p>
     </div>
   );
--- FE/haiyeo/src/pages/RewardShop.jsx
+++ FE/haiyeo/src/pages/RewardShop.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RewardShop() {
+  const [coupons, setCoupons] = useState(2);
   return (
-    <div className="RewardShop">
-      <p>리워드샵</p>
+    <div className="w-[512px]">
+      <div className="w-[512px] bg-[#B3F3F4] pb-10 pt-20">
+        <div>
+          <img
+            className="w-[200px] absolute mt-[-40px] ml-6"
+            alt="hanaEarth"
+            src="/img/hana_earth.png"
+          />
+        </div>
+        <div className="bg-white rounded-xl grid grid-cols-5 w-[460px] mx-auto pt-4 pb-2">
+          <div className="col-span-2"></div>
+          <div className="col-span-3">
+            <div className="font-bold text-3xl">GREEN SHOP</div>
+            <div>
+              <span className="text-[#3EC6C9]">초록빛</span>
+              <span> 당신의 노력을 모아</span>
+            </div>
+            <div>
+              <span>든든한 </span>
+              <span className="text-[#3EC6C9]">하나</span>
+              <span>의 상품으로!</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <img
+          className="w-16 ml-12 mt-4"
+          alt="starboy"
+          src="img/starStarboy.png"
+        />
+        <span className="ml-6 mt-9 text-lg">
+          나의 리워드 교환권 : {coupons} 개
+        </span>
+      </div>
     </div>
   );
 }
