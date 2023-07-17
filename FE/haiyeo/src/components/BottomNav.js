6f47db07954f5559721cb4332aa7d028e0143806
ljh991004 <ljh991004@naver.com>
2023-07-18 01:25:04 +0900

    FE/FEAT : css

--- FE/haiyeo/src/components/BottomNav.js
+++ FE/haiyeo/src/components/BottomNav.js
@@ -2,20 +2,14 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Nav.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHouse,
-  faStamp,
-  faQrcode,
-  faGift,
-  faUser,
-} from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faStamp, faQrcode, faGift, faUser } from "@fortawesome/free-solid-svg-icons";
 
 function BottomNav() {
   const [activeNav, setActiveNav] = useState(1); // 현재 활성화된 내비 버튼 상태
 
   return (
     <nav>
-      <div>
+      <div className=" font-sans">
         <NavLink to="/" onClick={() => setActiveNav(1)}>
           {activeNav === 1 ? (
             <div className="grid">
@@ -30,7 +24,7 @@ function BottomNav() {
           )}
         </NavLink>
       </div>
-      <div>
+      <div className=" font-sans">
         <NavLink to="/stamp" onClick={() => setActiveNav(2)}>
           {activeNav === 2 ? (
             <div className="grid">
@@ -49,13 +43,13 @@ function BottomNav() {
           )}
         </NavLink>
       </div>
-      <div className="grid">
+      <div className="grid  font-sans">
         <div className="mx-auto mb-[-28px]">
           <FontAwesomeIcon icon={faQrcode} color="#ababab" size="2x" />
         </div>
         <span className="text-sm mx-auto text-[#ababab]">QR스캔</span>
       </div>
-      <div>
+      <div className=" font-sans">
         <NavLink to="/rewardshop" onClick={() => setActiveNav(3)}>
           {activeNav === 3 ? (
             <div className="grid">
@@ -74,7 +68,7 @@ function BottomNav() {
           )}
         </NavLink>
       </div>
-      <div>
+      <div className=" font-sans">
         <NavLink to="/profile" onClick={() => setActiveNav(4)}>
           {activeNav === 4 ? (
             <div className="grid">
--- FE/haiyeo/src/pages/Result.jsx
+++ FE/haiyeo/src/pages/Result.jsx
@@ -30,12 +30,12 @@ function Result() {
   };
 
   return (
-    <div className="w-[100%]">
+    <div className="w-[100%] font-sans">
       <div className="w-10/12 mx-auto">
         <div className="grid grid-cols-2">
           <div className="pt-8">
             <span className="text-[#3EC6C9] font-bold text-3xl">부산</span>
-            <span className="text-[#898989] text-xs"> 으로의 ECO 여행</span>
+            <span className="text-[#898989] text-xs font-bold"> 으로의 ECO 여행</span>
           </div>
           <div className="pt-8 flex flex-row-reverse">
             <button onClick={moveToBack}>
@@ -43,34 +43,39 @@ function Result() {
             </button>
           </div>
         </div>
-        <div className="text-[18px] ml-1"> 8월 15일(수) ~ 8월 16일(목)</div>
+        <div className="text-[18px] ml-1 font-bold"> 7월 19일(수) ~ 7월 20일(목)</div>
       </div>
-      <div className="w-11/12 mx-auto grid grid-cols-4 text-[#898989] mt-10">
+      <div className="w-11/12 mx-auto grid grid-cols-4 text-[#898989] mt-6">
         <button onClick={() => changeCatetory("Lodge")}>
           <div>
             <img className="w-16 mx-auto mb-1" alt="lodgeStar" src="img/lodgeStar.png" />
           </div>
           <div>숙박</div>
+          {category === "Lodge" && <hr className="border-[#3EC6C9] border-2" />}
         </button>
         <button onClick={() => changeCatetory("Trans")}>
           <div>
             <img className="w-16 mx-auto" alt="lodgeStar" src="img/transStar.png" />
           </div>
           <div>교통</div>
+          {category === "Trans" && <hr className="border-[#3EC6C9] border-2" />}
         </button>
         <button onClick={() => changeCatetory("Food")}>
           <div>
             <img className="w-14 mx-auto mb-2" alt="lodgeStar" src="img/foodStar.png" />
           </div>
           <div>먹을거리</div>
+          {category === "Food" && <hr className="border-[#3EC6C9] border-2" />}
         </button>
         <button onClick={() => changeCatetory("Activity")}>
           <div>
             <img className="w-[50px] mx-auto" alt="lodgeStar" src="img/activityStar.png" />
           </div>
           <div>액티비티</div>
+          {category === "Activity" && <hr className="border-[#3EC6C9] border-2" />}
         </button>
       </div>
+      <hr className="border-[#E6F7F8] border-1" />
       <div className="w-full bg-[#F2FFFB]">{chooseCategory()}</div>
     </div>
   );
