2b70c9e998eaf45d260e69af306739f041e08eb6
ljh991004 <ljh991004@naver.com>
2023-07-18 02:01:46 +0900

    FE/FEAT : 마지막

--- FE/haiyeo/src/pages/SearchSchedule.jsx
+++ FE/haiyeo/src/pages/SearchSchedule.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 function SearchSchedule() {
-  // const [dateValue, setDateValue] = useState(new Date());
   const [endDate, setEndDate] = useState("");
   const [startDate, setStartDate] = useState("");
 
